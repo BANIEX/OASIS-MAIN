00c7d804a94bcb4f3ea425aa52e21644a7b0c995
baniex <banjibaniex1@gmail.com>
2023-07-03 09:54:08 +0100

    coming thru

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+// import { useState } from 'react'
+import Nav from './Components/Navbar/Nav'
 
 import './App.css'
 
@@ -7,6 +8,7 @@ function App() {
 
   return (
     <>
+    <Nav/>
       
     </>
   )
---
+++ src/Components/Navbar/Nav.jsx
@@ -0,0 +1,23 @@
+import logo from '../../image/logo.jpeg'
+import './nav.css'
+
+const Nav = () => {
+  return (
+    <div className='nav__con'>
+        <div>
+            <img className='logo' src={logo} alt='logo' />
+        </div>
+        <nav>
+            <ul className='nav__list'>
+                <li>Home</li>
+                <li>About Us</li>
+                <li>Contact Us</li>
+                <li>Support</li>
+                <li className='donate'>Donate</li>
+            </ul>
+        </nav>
+    </div>
+  )
+}
+
+export default Nav
