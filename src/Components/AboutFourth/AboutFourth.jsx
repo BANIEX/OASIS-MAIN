e3dbb7088eae2030b7715a37d3b48a086df5c5d8
baniex <banjibaniex1@gmail.com>
2023-07-23 23:57:50 +0100

    you say

--- src/Components/AboutFourth/AboutFourth.jsx
+++ src/Components/AboutFourth/AboutFourth.jsx
@@ -10,8 +10,33 @@ const AboutFourth = () => {
           <div className="about_fourth_top_third_text">Non Communicable Diseases(NCDs) kills 41 million people yearly which is equivalent to 71% of all deaths globally.</div>
         </div>
         <div className="about_fourth_middle">
+          <div className="about_fourth_middle_list">
+            <div className="about_fourth_bullets"></div><div>There is need for proper enlightenment on the non-communicable diseases, access to screening and treatment.
+  </div>
+          </div>
+
+          <div className="about_fourth_middle_list">
+            <div className="about_fourth_bullets"></div><div>There is need for proper enlightenment on the non-communicable diseases, access to screening and treatment.
+  </div>
+          </div>
+
+          <div className="about_fourth_middle_list">
+            <div className="about_fourth_bullets"></div><div>There is need for proper enlightenment on the non-communicable diseases, access to screening and treatment.
+  </div>
+          </div>
+
+          <div className="about_fourth_middle_list">
+            <div className="about_fourth_bullets"></div><div>There is need for proper enlightenment on the non-communicable diseases, access to screening and treatment.
+  </div>
+          </div>
+
         </div>
+
+
         <div className="about_fourth_bottom">
+          <div className="about_fourth_image_contaner">
+            <img className="about_fourth_image"></img>
+          </div>
         </div>
 
       </div>
