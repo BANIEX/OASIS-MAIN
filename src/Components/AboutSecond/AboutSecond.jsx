6384ecde7b3560e6f97f54cd77083cddc9ccff94
baniex <banjibaniex1@gmail.com>
2023-08-05 14:32:36 +0100

    hello

--- src/Components/AboutSecond/AboutSecond.jsx
+++ src/Components/AboutSecond/AboutSecond.jsx
@@ -1,6 +1,5 @@
-import "./AboutSecond.css"
-import Ellipse268 from '../../image/Ellipse268.png'
-
+import "./AboutSecond.css";
+import Ellipse268 from "../../image/Ellipse268.png";
 
 const AboutSecond = () => {
   return (
@@ -9,19 +8,20 @@ const AboutSecond = () => {
         <div className="about_second_subcontainer">
           <div className="about_second_left">
             <div className="about_second_left_maintext">More About Us</div>
-            <div className="about_second_left_subtext">Our passion was borne out of the need to reduce the high rates of death and enormous burden that stems from a ravaging invasion of the non communicable diseases in Nigeria.</div>
-
+            <div className="about_second_left_subtext">
+              Our passion was borne out of the need to reduce the high rates of
+              death and enormous burden that stems from a ravaging invasion of
+              the non communicable diseases in Nigeria.
+            </div>
           </div>
 
           <div className="about_second_right">
-          <img src={Ellipse268} alt='pic'/>
-
+            <img src={Ellipse268} alt="pic" />
           </div>
-
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default AboutSecond
+export default AboutSecond;
